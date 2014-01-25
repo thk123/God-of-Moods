1266f5532bd6e64eec6560b28ddcdd696895e52e
thk123 <thekileyenator@gmail.com>
2014-01-25 15:22:08 +0000

    Added life segment prefab

---
+++ Assets/Scripts/LifeSegment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class LifeSegment : MonoBehaviour {
+
+    bool selected;
+
+    MeshRenderer rendererer;
+
+    public Color32 colour
+    {
+        set
+        {
+            renderer.material.color = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        selected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
