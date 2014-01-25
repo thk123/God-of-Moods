3c3c4495c1d3941511bf822645ead209a4c97e33
thk123 <thekileyenator@gmail.com>
2014-01-25 12:14:47 +0000

    Faffed around trying to get stereographic projection working, stuck with orthographic

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class Draggable : MonoBehaviour {
 
+    
+
     bool beingDragged;
 
     Vector3 startingPosition;
 
     IDropBox onDropBox;
 
+    Matrix4x4 scale;
+    Matrix4x4 inverseScale;
+
+    public float scaleFactor;
+
+
 	// Use this for initialization
 	void Start () {
         beingDragged = false;
+
+        scale = Matrix4x4.Scale(scaleFactor * Vector3.one);
+        inverseScale = scale.inverse;
 	}
 	
 	// Update is called once per frame
@@ -19,7 +30,8 @@ public class Draggable : MonoBehaviour {
         if (beingDragged)
         {
             Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            transform.position = clickRay.GetPoint(2.0f);
+            Vector3 pos = clickRay.GetPoint(8.0f);
+            transform.position  = pos;
 
             if (Input.GetMouseButtonUp(0))
             {
@@ -33,6 +45,8 @@ public class Draggable : MonoBehaviour {
                 {
                     transform.position = startingPosition;
                 }
+
+                transform.localScale = inverseScale * transform.localScale;
             }
         }
         else
@@ -40,23 +54,43 @@ public class Draggable : MonoBehaviour {
             if (Input.GetMouseButtonDown(0))
             {
                 onDropBox = null;
-                Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-                // THIS IS NOT WORKING -- BEFORE WE WERE USING 3D RAY CAST, NOW NEED TO MODIFY TO 2D so not even a fecking ray cast
 
-                //RaycastHit info;
-                if (collider2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+                if (AttemptClickOrtho())
                 {
-                    //if (info.collider.gameObject == gameObject)
-                    {
-                        beingDragged = true;
-                        startingPosition = transform.position;
-                    }
+                    beingDragged = true;
+                    startingPosition = transform.position;
+
+                    transform.localScale = scale * transform.localScale;
                 }
             }
         }
         
 	}
+
+    bool AttemptClickOrtho()
+    {
+        
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return collider2D.OverlapPoint(pos);
+        
+    }
+
+    bool AttemptClickPersp()
+    {
+        Ray clickRay = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 20.0f));
+
+        RaycastHit info;
+        if (Physics.Raycast(clickRay, out info))
+        {
+            if (info.collider.gameObject.transform.parent.gameObject == gameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -65,7 +99,6 @@ public class Draggable : MonoBehaviour {
 
         if (dropBox != null)
         {
-            print("Found drop box");
             onDropBox = dropBox;
         }
         else
