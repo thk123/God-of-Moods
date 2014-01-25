afcd4cb83464d9351eb77d4a2889c92c86b3ab3d
thk123 <thekileyenator@gmail.com>
2014-01-25 12:48:54 +0000

    Implemented slide behaviour

---
+++ Assets/Scripts/Slide.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slide : MonoBehaviour {
+
+    Vector3 startPosition;
+    Vector3 endPosition;
+
+    float timeToTake;
+
+    float timePassed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(startPosition, endPosition, timePassed / timeToTake);
+	}
+}
