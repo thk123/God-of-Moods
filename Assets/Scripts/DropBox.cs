0c39eee90670c40ec04b851be4c73bc93558631a
thk123 <thekileyenator@gmail.com>
2014-01-26 01:10:12 +0000

    Added GUI elements

--- Assets/Scripts/DropBox.cs
+++ Assets/Scripts/DropBox.cs
@@ -18,6 +18,8 @@ public class DropBox : MonoBehaviour, IDropBox {
     public Texture2D canDropTexture;
     public Texture2D cantDropTexture;
 
+    MoodCard cardOnBox;
+
     public bool CanDrop
     {
         get 
@@ -29,13 +31,17 @@ public class DropBox : MonoBehaviour, IDropBox {
             internalCanDrop = value;
             if (internalCanDrop)
             {
-                renderer.material.mainTexture = canDropTexture;
+                //renderer.material.mainTexture = canDropTexture;
+                if (cardOnBox != null)
+                {
+                    Destroy(cardOnBox.gameObject);
+                }
             }
-            else
+            /*else
             {
                 renderer.material.mainTexture = cantDropTexture;
 
-            }
+            }*/
         }
     }
 
@@ -55,8 +61,11 @@ public class DropBox : MonoBehaviour, IDropBox {
     public void DropCard(MoodCard card)
     {
         manager.PlayCard(card.mood);
-        GameObject.Destroy(card.gameObject);
+        //GameObject.Destroy(card.gameObject);
 
+        card.transform.position = transform.position;
+        card.transform.Translate(Vector3.up);     
+        cardOnBox = card;
         CanDrop = false;
     }
 }
